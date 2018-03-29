f8141f4a5da94942c3da2c21e8b55f3a278b882f
pixelSunBro <pixelsunbro@gmail.com>
2018-03-29 22:56:31 +0300

    task2 again

--- HomeWorks/Task 2/src/com/company/Main.java
+++ HomeWorks/Task 2/src/com/company/Main.java
@@ -11,50 +11,50 @@ public class Main {
         while (true) {
             System.out.println("MENU");
             System.out.println("1. Указать диапазон и вывести на экран его четные числа");
-            System.out.println("2. Указать диапазон и узнать сумму его чисел");
+            System.out.println("2. Получит сумму введенных чисел");
+            System.out.println("3. Узнать сумму чисел введенного числа");
 
             int command = scanner.nextInt();
             switch (command) {
                 case 1: {
-                    System.out.println("Ведите первое число диапазон: ");
+                    System.out.println("Ведите первое число диапазона: ");
                     int firstNum = scanner.nextInt();
-                    System.out.println("Введите последние число диапазон: ");
+                    System.out.println("Введите последние число диапазона: ");
                     int lastNum = scanner.nextInt();
-                    int sumNum = firstNum + lastNum;
                     for (int i = firstNum; i <= lastNum; i++) {
                         if (i % 2 == 0) {
                             System.out.print(i + " ");
                         }
                     }
-                    System.out.println("");
-                    System.out.println("1. Показать сумму введенных чисел: ");
-                    System.out.println("2. Начать заново");
-                    int command2 = scanner.nextInt();
-                    switch (command2) {
-                        case 1: {
-                            System.out.println("Сумма введеных чисел равна: " + sumNum);
-                            break;
-                        }
-
-                        case 2:
-                            continue;
-                    }
+                    System.out.println();
                     break;
                 }
 
                 case 2: {
-                    System.out.println("Ведите первое число диапазон: ");
-                    int firstNum = scanner.nextInt();
-                    System.out.println("Введите последние число диапазон: ");
-                    int lastNum = scanner.nextInt();
-
-                    long b = 0;
-                    for (int i = firstNum; i <= lastNum; i++) {
-                        b = b + i;
-                        if (i == lastNum) {
-                            System.out.println(b);
+                    while (true) {
+                        System.out.println("Укажите количество чисел которые вы введете: ");
+                        int sumNum = scanner.nextInt();
+                        int sum = 0;
+                        for (int i = 0; i < sumNum; i++) {
+                            System.out.println("Введите число № " + (i + 1) + ": ");
+                            int number = scanner.nextInt();
+                            sum += number;
                         }
+                        System.out.println("Сумма введеных чисел равна: " + sum);
+                        break;
                     }
+                    break;
+                }
+
+                case 3: {
+                    System.out.println("Ведите число: ");
+                    int number = scanner.nextInt();
+                    int sum = 0;
+                    while (number > 0){
+                        sum += number % 10;
+                        number /= 10;
+                    }
+                    System.out.println(sum);
                     break;
                 }
 
