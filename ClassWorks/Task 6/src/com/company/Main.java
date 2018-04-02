f475ac62b413017fd2c6d8aa670ab4acea701615
Student9 <student9@it-park.ru>
2018-04-02 21:07:52 +0300

    add class work Task 6

---
+++ ClassWorks/Task 6/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner. nextInt();
+        RationalNumber rationalNumber = new RationalNumber(a , b);
+
+        rationalNumber.print();
+        System.out.println(rationalNumber);
+    }
+}
---
+++ ClassWorks/Task 6/src/com/company/RationalNumber.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class RationalNumber {
+    int a;
+    int b;
+
+    public RationalNumber () {
+        a = 0;
+        b = 1;
+    }
+    public Show (int a, int b) {
+        int result = a/b;
+    }
+
+    void print() {
+        int result = a/b;
+        return;
+    }
+}
+
+
